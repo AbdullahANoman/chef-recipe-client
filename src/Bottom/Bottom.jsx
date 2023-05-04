7448c73a06157e651edc915f0746df096c8ecb6e
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-05-04 22:21:19 +0600

    add a bottom section

---
+++ src/Bottom/Bottom.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Bottom = () => {
+  return (
+    <div className="my-container text-white border p-5 mt-5 ">
+      <p className="text-center uppercase text-2xl font-bold">Connect with us</p>
+      <div>
+        <div className="md:flex justify-center items-center gap-10 mt-10">
+          <p className="text-xl uppercase">
+            Please give me your email address , We will contact with you
+          </p>
+          <div className="md:flex gap-5">
+          <input
+            type="text"
+            placeholder="ENTER YOUR EMAIL"
+            className="input input-bordered text-black  w-full max-w-xs"
+          />
+          <button className="btn-primary">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bottom;
--- src/Layout/ChefCardsLayout/ChefCardsLayout.jsx
+++ src/Layout/ChefCardsLayout/ChefCardsLayout.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ChefsCards from '../../Components/ChefsCards/ChefsCards';
+import React from "react";
+import ChefsCards from "../../Components/ChefsCards/ChefsCards";
 
-import RightSide from '../../Components/RightSide/RightSide';
+import RightSide from "../../Components/RightSide/RightSide";
+import Bottom from "../../Bottom/Bottom";
 
-const ChefCardsLayout = ({chefsData}) => {
-    return (
-        <div className='grid   md:grid-cols-4 md:py-20 gap-10  my-container'>
-            <div className='col-span-3'>
-                <ChefsCards chefsData={chefsData}></ChefsCards>
-            </div>
-            <div>
-                <RightSide></RightSide>
-            </div>
+const ChefCardsLayout = ({ chefsData }) => {
+  return (
+    <>
+      <div className="grid   md:grid-cols-4 md:py-20 gap-10   my-container">
+        <div className="col-span-3">
+          <ChefsCards chefsData={chefsData}></ChefsCards>
         </div>
-    );
+        <div className=" col-span-1">
+          <RightSide></RightSide>
+        </div>
+      </div>
+      <div className="pb-10">
+        <Bottom></Bottom>
+      </div>
+    </>
+  );
 };
 
 export default ChefCardsLayout;
